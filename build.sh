39319b81fcdf6a1b42bf11678e0a0ae00dbee9ff
Vincent Tavernier <vince.tavernier@gmail.com>
2017-07-12 13:38:21 +0200

    Add deb build option

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+
+while getopts ":d" opt; do
+	case "$opt" in
+		d)
+			# Build deb using docker
+			docker build -t libshadertoy .
+
+			# Run container
+			docker run --rm -it -v $(pwd)/..:/app -v $HOME/.gnupg:/root/.gnupg \
+				libshadertoy:latest
+
+			# Quit
+			exit $?
+		;;
+	esac
+done
+
+# Else, default build
 ./configure.sh "$@"
 cd build
 make -j9
