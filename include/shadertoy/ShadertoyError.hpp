99a192ec4d8c8ba17f3fd96e2e6b2c0f3e202e4f
Vincent Tavernier <vince.tavernier@gmail.com>
2017-11-30 11:31:57 +0100

    Add ShadertoyError documentation

--- include/shadertoy/ShadertoyError.hpp
+++ include/shadertoy/ShadertoyError.hpp
@@ -7,10 +7,23 @@
 
 namespace shadertoy
 {
+/**
+ * Base error class for libshadertoy errors.
+ */
 class ShadertoyError : public std::runtime_error
 {
 public:
+	/**
+	 * Initializes a new instance of the ShadertoyError class.
+	 *
+	 * @param  what_arg Error message
+	 */
 	explicit ShadertoyError(const std::string &what_arg);
+	/**
+	 * Initializes a new instance of the ShadertoyError class.
+	 *
+	 * @param  what_arg Error message
+	 */
 	explicit ShadertoyError(const char *what_arg);
 };
 }
