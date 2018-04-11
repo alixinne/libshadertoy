3330d9a25f4c0286b347e49ff754e544b0c86db0
Vincent Tavernier <vince.tavernier@gmail.com>
2018-04-11 18:35:01 +0200

    Add Makefile to build packages in parallel

---
+++ Makefile
@@ -0,0 +1,19 @@
+ALL_DISTS=xenial stretch
+ALL_XENIAL_ARCHS=xenial-amd64 xenial-i386
+ALL_STRETCH_ARCHS=stretch-amd64 stretch-i386
+ALL_PKGS=$(ALL_XENIAL_ARCHS) $(ALL_STRETCH_ARCHS)
+
+IGNORE_TEST_FAILURES?=1
+SKIP_TESTS?=
+
+all: $(ALL_DISTS)
+
+xenial: $(ALL_XENIAL_ARCHS)
+
+stretch: $(ALL_STRETCH_ARCHS)
+
+$(ALL_PKGS):
+	./buildpackage.sh $@
+
+.PHONY: all $(ALL_DISTS) $(ALL_PKGS)
+
---
+++ buildpackage.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Get the build directory
+LIBDIRECTORY="$(pwd)"
+
+# Grab the current version number from the rules file
+LIBVERSION=$(head -n 1 debian/changelog | awk '{gsub("[()]","",$2); print $2}')
+
+echo "[==== BUILDING v$LIBVERSION ====]" >&2
+
+# We need to be in the parent directory
+cd "$(dirname "$BASH_SOURCE")/.."
+
+version_suffix () {
+	if [ "$1" = "stretch" ]; then
+		echo -n "debian9"
+	elif [ "$1" = "xenial" ]; then
+		echo -n "ubuntu16"
+	elif [ "$1" = "trusty" ]; then
+		echo -n "ubuntu14"
+	else
+		exit 1
+	fi
+}
+
+DISTRIBUTION=$(echo -n "$1" | cut -d- -f1)
+ARCH=$(echo -n "$1" | cut -d- -f2)
+
+if [ "x$DISTRIBUTION" = "x" ] || [ "x$ARCH" = "x" ]; then
+	exit 2
+fi
+
+mkdir -p libshadertoy-$LIBVERSION-$DISTRIBUTION
+
+echo "[==== BUILDING $DISTRIBUTION-$ARCH ====]" >&2
+if [ "$ARCH" = "amd64" ]; then
+	ARCHALL_ARG='--arch-all'
+else
+	ARCHALL_ARG='--no-arch-all'
+fi
+
+(cd $LIBDIRECTORY && sbuild --no-apt-update --no-apt-upgrade \
+							--no-apt-clean --resolve-alternatives \
+							-d $DISTRIBUTION \
+							--arch $ARCH \
+							$ARCHALL_ARG \
+							--append-to-version "-$(version_suffix $DISTRIBUTION)"
+)
+
+RESULT="$?"
+if [ "$RESULT" -ne "0" ]; then
+	echo "[==== BUILD FAILED FOR $DISTRIBUTION-$ARCH ====]" >&2
+	exit 1
+fi
+
+echo "[==== MOVING ARTIFACTS $DISTRIBUTION-$ARCH ====]" >&2
+CHANGES_FILE=libshadertoy_$LIBVERSION-$(version_suffix $DISTRIBUTION)_$ARCH.changes
+BUILD_ARTIFACTS=$(awk '/^Files:/{a=1;next}/^$/{a=0}{if(a)print $NF}' "$CHANGES_FILE")
+mv $BUILD_ARTIFACTS $CHANGES_FILE libshadertoy-$LIBVERSION-$DISTRIBUTION
+
+if [ "x$SKIP_TESTS" = "x" ]; then
+	echo "[==== TESTING ARTIFACTS $DISTRIBUTION-$ARCH ====]" >&2
+	(cd $LIBDIRECTORY &&
+		autopkgtest ../libshadertoy-$LIBVERSION-$DISTRIBUTION/libshadertoy*-$(version_suffix $DISTRIBUTION)_$ARCH.deb \
+		../libshadertoy-$LIBVERSION-$DISTRIBUTION/libshadertoy*-$(version_suffix $DISTRIBUTION)_all.deb \
+		-- schroot $DISTRIBUTION-$ARCH-sbuild)
+	if [ "$?" -ne "0" ]; then
+		echo "[==== TESTS FAILED FOR $DISTRIBUTION-$ARCH ====]" >&2
+		if [ "x$IGNORE_TEST_FAILURES" = "x" ]; then
+			exit 2
+		fi
+	fi
+fi
+
+echo "[==== DONE ====]" >&2
+exit 0
+
