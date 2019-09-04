071d983695abf3dab3a1541a440239776b67724a
Vincent Tavernier <vince.tavernier@gmail.com>
2019-09-04 11:02:51 +0200

    Output to stdout on emscripten

--- src/base/src/utils/log.cpp
+++ src/base/src/utils/log.cpp
@@ -10,7 +10,11 @@ std::shared_ptr<spd::logger> log::shadertoy()
 	if (!initialized_)
 	{
 		initialized_ = true;
+#if EMSCRIPTEN
+		return spd::stdout_color_st("shadertoy");
+#else
 		return spd::stderr_color_st("shadertoy");
+#endif
 	}
 
 	return spd::get("shadertoy");
